90335825ba93b69bdf7e4a356621bf85f73a4462
Jakob Van Alstine <vanalstinejakob@gmail.com>
2025-07-03 16:22:02 +0200

    make error handling tidy

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 # main.py
 
 import sys
+import os.path
 from stats import word_count
 from stats import chr_count
 from stats import sorted_list_of_chr_dict
 
 def main():
     if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+    if not os.path.exists(sys.argv[1]):
         print("Usage: python3 main.py <path_to_book>")
         sys.exit(1)
 
@@ -21,20 +26,23 @@ def main():
         print(e)
 
     print("============ BOOKBOT ============")
+
     print(f"Analyzing book found at {path}...")
+
     print("----------- Word Count ----------")
+
     print(f"Found {w_count} total words")
+
     print(f"--------- Character Count -------")
+
     for entry in sorted_dicts:
         if entry['char'].isalpha():
             print(f"{entry['char']}: {entry['num']}")
+
     print("============= END ===============")
 
 
 def get_text(path):
-    if type(path) != str:
-        raise TypeError("Error: not a string")
-
     with open(path) as f:
         return f.read()
 
--- stats.py
+++ stats.py
@@ -1,19 +1,11 @@
 # stats.py
 
 def word_count(text):
-    if type(text) != str:
-        raise TypeError("Error: not a string")
-
     return len(text.split())
 
 def chr_count(text):
-    if type(text) != str:
-        raise TypeError("Error: not a string")
-
-    text = text.lower()
-
     chr_set = set()
-    for chr in text:
+    for chr in text.lower():
         chr_set.add(f'{chr}')
 
     chr_dict = {}
@@ -26,17 +18,15 @@ def chr_count(text):
     return chr_dict
 
 def sorted_list_of_chr_dict(chr_dict):
-    if type(chr_dict) != dict:
-        raise TypeError("Error: not a dictionary")
-
-    def sort_on(items):
-        return items["num"]
-
     list_of_dict = []
     for dict_key in chr_dict:
         dict_value = chr_dict[dict_key]
         list_of_dict.append({"char": f'{dict_key}', "num": dict_value})
 
+    def sort_on(items):
+        return items["num"]
+
     list_of_dict.sort(reverse=True, key=sort_on)
+
     return list_of_dict
 
