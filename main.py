ccd639b42d6222d1f468f1628a081f455a337711
Jakob Van Alstine <vanalstinejakob@gmail.com>
2025-07-03 13:48:22 +0200

    add graceless prototype of character counting

--- main.py
+++ main.py
@@ -1,18 +1,21 @@
 # main.py
 
 from stats import word_count
+from stats import chr_count
 
 def main():
     path = "./books/frankenstein.txt"
 
     try:
         text = get_text(path)
-        count = word_count(text)
+        w_count = word_count(text)
+        chr_dict = chr_count(text)
     except Exception as e:
         print(e)
         raise
 
-    print(f"{count} words found in the document")
+    print(f"{w_count} words found in the document")
+    print(chr_dict)
 
 
 def get_text(path):
--- stats.py
+++ stats.py
@@ -6,3 +6,19 @@ def word_count(text):
 
     return len(text.split())
 
+def chr_count(text):
+    text = text.lower()
+
+    chr_set = set()
+    for chr in text:
+        chr_set.add(f'{chr}')
+
+    chr_dict = {}
+    for chr in chr_set:
+        chr_dict[chr] = 0
+
+    for chr in text.lower():
+        chr_dict[chr] += 1
+
+    return chr_dict
+
