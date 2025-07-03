bac9fffb13cde9a0d87b6907b259dc1579457613
Jakob Van Alstine <vanalstinejakob@gmail.com>
2025-07-03 17:46:06 +0200

    tidy up main.py (note: broken  until stats.py is tidy)

--- main.py
+++ main.py
@@ -1,50 +1,52 @@
+
 # main.py
 
 import sys
 import os.path
-from stats import word_count
-from stats import chr_count
-from stats import sorted_list_of_chr_dict
+from stats import (
+    word_count,
+    char_count,
+    sort_char_counts,
+)
+
 
 def main():
     if len(sys.argv) != 2:
-        print("Usage: python3 main.py <path_to_book>")
+        print("Usage: python3 bookbot.py <path_to_book>")
         sys.exit(1)
 
     if not os.path.exists(sys.argv[1]):
-        print("Usage: python3 main.py <path_to_book>")
+        print("Usage: python3 bookbot.py <path_to_book>")
         sys.exit(1)
+
 
     path = sys.argv[1]
 
+
+    with open(path) as f:
+        text = f.read()
+
+
     try:
-        text = get_text(path)
-        w_count = word_count(text)
-        chr_dict = chr_count(text)
-        sorted_dicts = sorted_list_of_chr_dict(chr_dict)
+        count = word_count(text)
+        char_count_dict = char_count(text)
+        sorted_char_counts = sort_char_counts(char_count_dict)
     except Exception as e:
         print(e)
+        sys.exit(1)
+
 
     print("============ BOOKBOT ============")
-
     print(f"Analyzing book found at {path}...")
-
     print("----------- Word Count ----------")
-
-    print(f"Found {w_count} total words")
-
+    print(f"Found {count} total words")
     print(f"--------- Character Count -------")
 
-    for entry in sorted_dicts:
+    for entry in sorted_char_counts:
         if entry['char'].isalpha():
             print(f"{entry['char']}: {entry['num']}")
 
     print("============= END ===============")
-
-
-def get_text(path):
-    with open(path) as f:
-        return f.read()
 
 main()
 
